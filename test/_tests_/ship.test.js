ee54645cf4fb5b91af965f17cb1a9ad0af8ebfb7
odenra <ilkerkaraoden@gmail.com>
2023-04-26 14:57:47 +0300

    updateSomeTests

---
+++ test/_tests_/ship.test.js
@@ -0,0 +1,15 @@
+const Ship = require('../src/Ship');
+
+
+
+describe('Ship', () =>{
+    it('can be instantiated', () => {
+        expect(new Ship()).toBeInstanceOf(Object);
+    });
+
+    it('has a starting port', () => {
+        const ship = new Ship('Dover');
+
+        expect(ship.startingPort).toBe('Dover');
+    });
+});
